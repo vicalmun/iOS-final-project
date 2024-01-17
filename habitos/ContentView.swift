06adbcf5e6cebe027479bbf3851f84452288d2fb
Víctor Alba <victoram97@gmail.com>
2024-01-17 18:35:23 +0100

    Initial Commit

---
+++ habitos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  habitos
+//
+//  Created by Víctor Alba on 17/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ habitos/habitosApp.swift
@@ -0,0 +1,17 @@
+//
+//  habitosApp.swift
+//  habitos
+//
+//  Created by Víctor Alba on 17/1/24.
+//
+
+import SwiftUI
+
+@main
+struct habitosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
